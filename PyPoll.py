ff26b2e1914376f5636639cd825280558372fb36
Moonem <m.a.moonem@gmail.com>
2021-03-29 16:47:04 -0500

    Adding the election audit code. git push git status didr dir git push exit cd..

--- PyPoll.py
+++ PyPoll.py
@@ -15,6 +15,20 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+# 1. Initialize total vote counter.
+total_votes = 0
+
+# Declaring a new list 
+candidate_options = []
+
+# Declare an emply dictionary.
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file
 with open(file_to_load, 'r') as election_data:
 
@@ -24,10 +38,64 @@ with open(file_to_load, 'r') as election_data:
 
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
-    # Print each row in the CSV file.
-    #for row in file_reader:
-        #print(row[i])
+    ##print(headers)
+
+    # 2. Add to total vote count.
+    for row in file_reader:
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate name doesn't match existing candidate..
+        if candidate_name not in candidate_options:
+            # Add to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to the candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Determin the percentage of votes for each candidate by looping through the counts.
+# Iterate through the candidate list.
+for candidate_name in candidate_votes:
+
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # Calculate the percentage of vote.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate.
+    # Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent = vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+# Print out the winning candidate, vote count and percentage of votes.
+winning_candidate_summary = (
+    f"--------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percetage: {winning_percentage:.1f}\n"
+    f"--------------------------\n"
+)
+print(winning_candidate_summary)
+
+# Print total votes.
+##print(total_votes)
+    
+# Print the candidate list.
+##print(candidate_options)
+
+# Print candidate's vote.
+##print(candidate_votes)
 
 
 
---
+++ Python_practice.py
@@ -0,0 +1,9 @@
+#counties_tuple = ("Arapahoe","Denver","Jefferson")
+#counties_dict = {"Arapahoe":422829, "Denver": 463353, "Jefferson": 432438}
+#voting_data = [{'county': 'Arapahoe', 'registered_voters': 422829}, {'county': 'Denver', 'registered_voters': 463353}, {'county': 'Jefferson', 'registered_voters': 432438}]
+#for county_dict in voting_data:
+   #print(f"{county_dict['county']} county has {county_dict['registered_voters']} registered voters. ")
+    
+import datetime
+now = datetime.datetime.now()
+print(now)
